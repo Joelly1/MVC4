faa903df08a1b11de5dd927d311eeeddb489481b
joel <olugaabiola18@gmail.com>
2024-01-07 15:40:41 +0100

    Second Commit

---
+++ MVC4/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using MVC4.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MVC4.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			//Connect to the database
+			SampleDb db = new SampleDb();
+			//Retrieve departments, and build selectlist
+			List<SelectListItem> selectListItems = new List<SelectListItem>();
+			foreach (Department department in db.Departments)
+			{
+				SelectListItem selectListItem = new SelectListItem
+				{
+					Text = department.Name,
+					Value = department.Id.ToString(),
+					Selected = department.IsSelected.HasValue ? department.IsSelected.Value : false
+				};
+				selectListItems.Add(selectListItem);
+			};
+			ViewBag.Departments = selectListItems;
+			return View();
+		}
+
+		public ActionResult Index1()
+		{
+			Company company = new Company("BeeBee");
+			ViewBag.Departments = new SelectList(company.Departments, "Id", "Name");
+			ViewBag.CompanyName = company.CompanyName;
+
+			return View();
+		}
+
+		public ActionResult Index2()
+		{
+			Company company = new Company("BeeBee");
+
+			return View(company);
+		}
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+}
---
+++ MVC4/Models/Company.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC4.Models
+{
+	public class Company
+	{
+		private string _name;
+		
+		public Company(string name)
+		{
+			this._name = name;
+		}
+
+		public List<Department> Departments
+		{
+			get
+			{
+				SampleDb db = new SampleDb();
+				return db.Departments.ToList();
+
+			}
+		}
+		public string CompanyName
+		{
+			get
+			{
+				return _name;
+			}
+			set
+			{
+				_name = value;
+			}
+		}
+	}
+}
